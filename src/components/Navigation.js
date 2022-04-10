5140096836cf41710bd1ed9adc37b542037e8c21
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-04-10 17:21:05 -0500

    Changing Title in index.html. Modifying src attribute in Footer.js to try loading the images correctly. Adding bootstrap classes to ul, li and a tags in Navigation.js file.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav>
-            <ul>
-                <li><a className="section-links" href="#about-me">About Me</a></li>
-                <li><a className="section-links" href="#portfolio">Portfolio</a></li>
-                <li><a className="section-links" href="#contact">Contact</a></li>
-                <li><a className="section-links" href="#resume">Resume</a></li>
+            <ul className="nav">
+                <li className="nav-item"><a className="nav-link" href="#about-me">About Me</a></li>
+                <li className="nav-item"><a className="nav-link" href="#portfolio">Portfolio</a></li>
+                <li className="nav-item"><a className="nav-link" href="#contact">Contact</a></li>
+                <li className="nav-item"><a className="nav-link" href="#resume">Resume</a></li>
             </ul>
         </nav>
     );
