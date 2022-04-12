698e1dca6aa15c8cd8b072b26c5bff6af470908c
josevidmal <92562836+josevidmal@users.noreply.github.com>
2022-04-11 23:38:45 -0500

    Adding React Icons package to package.json. Modifying code in Footer.js, importing GitHub, LinkedIn and Stack Overflow icons and setting them in the return value of Footer().

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import GitHubLogo from '../assets/github-logo.png';
-import LinkedInLogo from '../assets/linkedin-logo.png';
-import StackOverflowLogo from '../assets/stack-overflow-logo.png';
+import { FaGithub, FaLinkedin, FaStackOverflow } from 'react-icons/fa';
 
 const Footer = () => {
     return (
         <footer>
             <ul className="nav">
-                <li className="nav-item"><a className="nav-link" id="github" href="https://github.com/josevidmal" target="_blank"><img src={GitHubLogo}/></a></li>
-                <li className="nav-item"><a className="nav-link" id="linkedin" href="https://www.linkedin.com/in/jose-vidal-maza-alonso-73477646/" target="_blank"><img src={LinkedInLogo} /></a></li>
-                <li className="nav-item"><a className="nav-link" id="stack-overflow" href="https://stackoverflow.com/users/17156054/jos%c3%a9-vidal" target="_blank"><img src={StackOverflowLogo}/></a></li>
+                <li className="nav-item"><a className="nav-link" id="github" href="https://github.com/josevidmal" target="_blank"><FaGithub /></a></li>
+                <li className="nav-item"><a className="nav-link" id="linkedin" href="https://www.linkedin.com/in/jose-vidal-maza-alonso-73477646/" target="_blank"><FaLinkedin /></a></li>
+                <li className="nav-item"><a className="nav-link" id="stack-overflow" href="https://stackoverflow.com/users/17156054/jos%c3%a9-vidal" target="_blank"><FaStackOverflow /></a></li>
             </ul>
         </footer>
     );
